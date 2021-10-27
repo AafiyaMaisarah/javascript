49a4b283c5f221c25ad41e2331f56b5b925a963f
AafiyaMaisarah <aafiyamaisarah@gmail.com>
2021-10-27 23:53:50 +0530

    javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+var x="Aafiya";
+console.log( typeof x);
+var b=4;
+console.log(typeof b)
+var a;
+console.log(typeof a);
+var e=true;
+console.log(typeof e);
+var f=null;
+console.log(typeof f);
+var g=Symbol("id");
+console.log(typeof g);
+console.log(z);
+var z;
+var c=2;
+console.log(c);
+var d=4;
+var h="5"
+console.log(d+h);
+console.log(4+5);
